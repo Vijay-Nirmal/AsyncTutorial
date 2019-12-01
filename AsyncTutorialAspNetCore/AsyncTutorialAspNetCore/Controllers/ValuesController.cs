0432f8f85913d63c9501d371f110dc8a96365d0f
Vijay Nirmal <pon.vijaynirmal@outlook.com>
2019-12-02 01:37:32 +0530

    Initial commit

---
+++ AsyncTutorialAspNetCore/AsyncTutorialAspNetCore/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AsyncTutorialAspNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("sleep")]
+        public ActionResult<IEnumerable<string>> Sleep()
+        {
+            Thread.Sleep(5000);
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        [Route("delay-wait")]
+        public ActionResult<IEnumerable<string>> DelayWait()
+        {
+            // Sync over Async
+            Task.Delay(5000).Wait();
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("run-sleep")]
+        public async Task<string> RunSleep()
+        {
+            // Async over Sync
+            await Task.Run(() => Thread.Sleep(5000));
+            return "Hello World";
+        }
+
+        [HttpGet]
+        [Route("async")]
+        public async Task<ActionResult<IEnumerable<string>>> GetAsync()
+        {
+            // Sync over Async
+            await Task.Delay(5000);
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ AsyncTutorialAspNetCore/AsyncTutorialAspNetCore/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AsyncTutorialAspNetCore
+{
+    public class Program
+    {
+        public static int Requests;
+
+        public static void Main(string[] args)
+        {
+            new Thread(ShowThreadStats)
+            {
+                IsBackground = true
+            }.Start();
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureLogging(builder =>
+            {
+                builder.ClearProviders();
+            })
+            .UseStartup<Startup>();
+
+        private static void ShowThreadStats(object _)
+        {
+            ThreadPool.SetMinThreads(2, 50);
+            ThreadPool.SetMaxThreads(10, 100);
+            while (true)
+            {
+                ThreadPool.GetAvailableThreads(out var workerThread, out var completionPortThreads);
+                // ThreadPool.GetMinThreads(out var minWorkerThread, out var minCompletionPortThreads);
+                ThreadPool.GetMaxThreads(out var maxWorkerThread, out var maxCompletionPortThreads);
+
+                Console.WriteLine($"Request -> {Requests} || WorkerThread -> Available: {workerThread} --- Active: {maxWorkerThread - workerThread} || CompletionPortThreads -> Available: {completionPortThreads} --- Active: {maxCompletionPortThreads - completionPortThreads}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
